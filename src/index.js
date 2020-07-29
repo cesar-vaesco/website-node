f352d9eacb175d1522d344bd763322f0300cc1d5
Cesar <cesarvargasescorcia@gmail.com>
2020-07-29 11:40:54 -0500

    web-node: configurando para poder renderizar vistas html usando sintaxis de ejs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const colors = require('colors');
 //Settings - configuraciones
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views')); //Se le indica a node donde va a ir para consultar las vistas y renderizarlas
+//Decirle a node que va a leer las vistas con extensión html y las va arenderizar como ejs
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
 //middlewares
 
 
@@ -19,4 +22,5 @@ app.use(require('./routes/index'));
 // listening the server Inicializando el servidor
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`.random);
+
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {
+    res.render('index.html', {
         title: 'First webSite'
     });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact', {
+    res.render('contact.html', {
         title: 'Contact'
     });
 });
