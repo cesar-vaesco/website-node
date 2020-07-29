018c71a5e366caef739bf55224dc6fe5ea9a8962
Cesar <cesarvargasescorcia@gmail.com>
2020-07-29 01:07:49 -0500

    web-node: configurando para usar ejs en el proyecto

--- src/index.js
+++ src/index.js
@@ -5,24 +5,21 @@ const colors = require('colors');
 
 //Settings - configuraciones
 app.set('port', process.env.PORT || 3000);
-
+app.set('views', path.join(__dirname, 'views')); //Se le indica a node donde va a ir para consultar las vistas y renderizarlas
+app.set('view engine', 'ejs');
 //middlewares
 
 
 //routes
 app.get('/', (req, res) => {
-    // /** Identificando la ruta de la vista */
-    // console.log(__dirname + '/views/index.html');
-    // console.log(path.join(__dirname, '/views/index.html'));
-    const indexVista = path.join(__dirname, '/views/index.html');
-    res.sendFile(indexVista);
+    res.render('index');
 
 });
 
 //static files
 
 
-// listening tje server Inicializando el servidor
+// listening the server Inicializando el servidor
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`.random);
 })
